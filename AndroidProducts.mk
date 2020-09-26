25b188efe77706b183f8b9be9e63ee852c865d36
Jabiyeff <cebiyevanar@gmail.com>
2020-09-26 17:48:20 +0400

    santoni: Revenge OS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
+    $(LOCAL_DIR)/revengeos_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
+    revengeos_santoni-userdebug \
+    revengeos_santoni-user \
+    revengeos_santoni-eng
